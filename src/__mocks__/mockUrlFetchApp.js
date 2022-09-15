db99140d3764a7963a91c585b1c36b3856b83bda
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-09-15 15:24:29 +0900

    GASのクラス`UrlFetchApp`のmock定義

---
+++ src/__mocks__/mockUrlFetchApp.js
@@ -0,0 +1,39 @@
+/**
+ * UrlFetchApp class
+ * @see https://developers.google.com/apps-script/reference/url-fetch/url-fetch-app?hl=en
+ */
+class MockUrlFetchApp {
+  /**
+   * Mocks a request to fetch a URL using optional advanced parameters.
+   * @param {string} url The URL to fetch.
+   * @param {*} params The optional JavaScript object specifying advanced parameters as defined in below web page.
+   * @return {*} The input URL and parameters as a JavaScript object.
+   * @see https://developers.google.com/apps-script/reference/url-fetch/url-fetch-app?hl=en#fetchurl,-params
+   */
+  static fetch(url, params = {}) {
+    return new MockHTTPResponse(url, params);
+  }
+}
+
+/**
+ * HTTPResponse class
+ * @see https://developers.google.com/apps-script/reference/url-fetch/http-response?hl=en
+ */
+class MockHTTPResponse {
+  /**
+   * @param {string} url
+   * @param {*} params
+   */
+  constructor(url, params) {
+    this.url = url;
+    this.params = params;
+  }
+  getContentText() {
+    return JSON.stringify({
+      url: this.url,
+      params: this.params,
+    });
+  }
+}
+
+module.exports = { MockUrlFetchApp };
